6538de2e233944e5aa8fbb13281e36539d6f009a
austin-do <43225674+austin-do@users.noreply.github.com>
2019-03-25 20:37:38 -0400

    CODE: split_users() added.

--- csv_reader.py
+++ csv_reader.py
@@ -1,26 +1,36 @@
 import csv
 from datetime import datetime
 
-
 def main():
+	users = split_users()
+	time_find()
+	for row in users[0]:
+		print(row[1])
+	
+def split_users():
 	user = ""
-
+	users = []
+	count = -1
+	
 	with open('text21.csv') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		
 		for row in csv_reader:
-			#print(f'\t{row[0]} {row[1]} {row[2]} {row[3]}')
 			if row[1] != user:
-				user = row[1]
 				print(f'new user: {row[1]}')
+				users.append([])
+				user = row[1]
+				count +=1
+				users[count].append(row)
 			else:
-				time_find()
-				
+				users[count].append(row)
+	return users
+	
 def time_find():
 	s1 = '14:51:24'
 	s2 = '14:51:44'
 	FMT = '%H:%M:%S'
 	tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
-	print(tdelta)
+	return(tdelta)
 
 main()
