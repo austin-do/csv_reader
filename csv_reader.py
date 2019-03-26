5875915b9a722e8ad13f6242f4336ef4637b405e
austin-do <43225674+austin-do@users.noreply.github.com>
2019-03-26 14:43:14 -0400

    CODE: find_login_times() added. Minor Refactoring

--- csv_reader.py
+++ csv_reader.py
@@ -3,9 +3,7 @@ from datetime import datetime
 
 def main():
 	users = split_users()
-	time_find()
-	for row in users[0]:
-		print(row[1])
+	find_login_times(users)
 
 #Puts users into 2 dimensional array with all 1st user log data at arr[0]
 def split_users():
@@ -26,12 +24,24 @@ def split_users():
 				users[count].append(row)
 	return users
 
+def find_login_times(users):
+	pass_entered = []
+	pass_submitted = []
+	total_time = "00:00:00"
+	for row in users[0]:
+		if row[5] == "enter":
+			pass_entered = pass_submitted
+			pass_submitted = row
+		if row[5] == "login":
+			time_find(pass_entered[0],pass_submitted[0])
+	
 #Subtracts and returns two time stamps
-def time_find():
+def time_find(total_time, last_enter):
 	s1 = '14:51:24'
 	s2 = '14:51:44'
 	FMT = '%H:%M:%S'
 	tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+	print(total_time + " " + last_enter + " ")
 	return(tdelta)
 
 main()
